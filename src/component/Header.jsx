d44aa2327354e67f98fb9d009e79a157be0f881b
zahidi14 <d.zahidi1412@gmail.com>
2023-04-18 09:18:31 +0800

    some fix

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,9 +1,10 @@
-import { useState } from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { Link } from "react-router-dom";
-const Header = () => {
-    const [isResp, setNav] = useState(true);
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faMugHot } from '@fortawesome/free-solid-svg-icons';
 
+import { Link } from "react-router-dom";
+import {Home ,User, Archive, Smile} from "react-feather";
+
+const Header = () => {
 
 
     return (
@@ -11,10 +12,10 @@ const Header = () => {
             <header>
                 <nav>
                     <ul>
-                        <li className="nav-item"><Link to="/" ><FontAwesomeIcon icon="fa-solid fa-house" /><span>Home</span></Link></li>
-                        <li className="nav-item"><Link to="/about" ><span>About</span></Link></li>
-                        <li className="nav-item"><Link to="/project" ><span>Project</span></Link></li>
-                        <li className="nav-item"><Link to="/contact" ><span>Contact</span></Link></li>
+                        <li className="nav-item"><Link to="/" ><FontAwesomeIcon icon={ faMugHot} /><span>Home</span></Link></li>
+                        <li className="nav-item"><Link to="/about" ><Smile /><span>About</span></Link></li>
+                        <li className="nav-item"><Link to="/project" ><Archive /><span>Project</span></Link></li>
+                        <li className="nav-item"><Link to="/contact" ><User /><span>Contact</span></Link></li>
                     </ul>
                 </nav>
             </header>
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,7 +1,10 @@
+import backg from "../asset/HeroBackg.svg";
+
 const Hero = () => {
 
     return (
-        <div className="hero">
+        <div className="hero" style={{ background: `url(${backg}) no-repeat center center fixed`, 
+        backgroundSize: 'cover'  }}>
             <section id="hero">
                 <h1>Welcome to My Portfolio</h1>
                 <p>I am a web developer who specializes in creating beautiful and functional websites and applications.</p>
