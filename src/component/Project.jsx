dbfba675d313f3bc8b05fff73be14c682a3c3bf1
zahidi14 <d.zahidi1412@gmail.com>
2023-04-23 11:05:43 +0800

    json

--- src/component/Project.jsx
+++ src/component/Project.jsx
@@ -1,28 +1,89 @@
 const Project = () => {
 
     return (
-        <div className="project">
-            <section id="projects">
-                <h2>Projects</h2>
-                <div className="project-list">
-                    <div className="project">
-                        <h3>Project Title</h3>
-                        <p>Project description goes here.</p>
-                        <a href="#">View Project</a>
+        <section id="project" class="project">
+        <div class="container">
+            <div class="header">
+                <h2>Project</h2>
+             </div>
+             <div class="row">
+                <div class="card-wrap">
+                    <div class="col">
+                       <div class="hov-card fm" >
+                           
+                             <div class="card-content">
+                                <h3>Base Apparel Coming Soon Page</h3>
+                                <h4>HTML CSS JS</h4>
+                                <p>Simple Landing Page</p>
+                                <a target="_blank" rel="noopener noreferrer" href="FeMentor/fm1/">Visit</a>
+                             </div>
+                       </div>
                     </div>
-                    <div className="project">
-                        <h3>Project Title</h3>
-                        <p>Project description goes here.</p>
-                        <a href="#">View Project</a>
+                    <div class="col">
+                          <div class="hov-card advGen">
+                               
+                                <div class="card-content">
+                                   <h3>Advice Generator</h3>
+                                   <h4>HTML SASS JS API </h4>
+                                   <p>Advice Generator using api</p>
+                                   <a target="_blank" rel="noopener noreferrer" href="FeMentor/advice/">Visit</a>
+                                </div>
+                          </div>
+                       </div>
+                       <div class="col">
+                             <div class="hov-card intro">
+                                 
+                                   <div class="card-content">
+                                      <h3>Project Tracking Intro Component</h3>
+                                      <h4>HTML CSS JS</h4>
+                                      <p>layout and code challenge from Front End Mentor</p>
+                                      <a target="_blank" rel="noopener noreferrer" href="FeMentor/intro/">Visit</a>
+                                   </div>
+                             </div>
+                          </div>
+                 </div>
+             </div>
+             <div class="row">
+                <div class="card-wrap">
+                    <div class="col">
+                       <div class="hov-card fylo" >
+                           
+                             <div class="card-content">
+                                <h3>Fylo Dark Theme</h3>
+                                <h4>HTML CSS JS</h4>
+                                <p>Simple Responsive design web page</p>
+                                <a target="_blank" rel="noopener noreferrer" href="FeMentor/fylo-dark/">Visit</a>
+                             </div>
+                       </div>
                     </div>
-                    <div className="project">
-                        <h3>Project Title</h3>
-                        <p>Project description goes here.</p>
-                        <a href="#">View Project</a>
-                    </div>
-                </div>
-            </section>
+                    <div class="col">
+                          <div class="hov-card hud">
+                               
+                                <div class="card-content">
+                                   <h3>Huddle Landing Page</h3>
+                                   <h4>HTML CSS</h4>
+                                   <p>Simple Landing Page</p>
+                                   <a target="_blank" rel="noopener noreferrer" href="FeMentor/hud/">Visit</a>
+                                </div>
+                          </div>
+                       </div>
+                       <div class="col">
+                             <div class="hov-card book">
+                            
+                                   <div class="card-content">
+                                      <h3>Simple Bookmark Manager</h3>
+                                      <h4>HTML CSS JS</h4>
+                                      <p>Responsive design using HTML & CSS</p>
+                                      <a target="_blank" rel="noopener noreferrer" href="FeMentor/book/">Visit</a>
+                                   </div>
+                             </div>
+                          </div>
+                 </div>
+             </div>
+            
         </div>
+       
+    </section>
     )
 }
 
